ec31845acfbd3794f75836d3ca308b42d01e535f
Akhilesh26 <akhileshpimple3@gmail.com>
2024-10-01 21:08:22 +0530

    create folder for react components

---
+++ Frontend/src/Components/SideBar.jsx
@@ -0,0 +1,15 @@
+const SideBar =  () =>{
+    
+    return (
+        <div>
+            <div>
+                All Operations
+            </div>
+            <button>Create New Result Holder</button>
+            <button>Update Existing Result Holder</button>
+            <button>Delete Exiting Result Holder</button>
+        </div>
+    )
+};
+
+export default SideBar;
