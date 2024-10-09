fba158029a01fd996e903b61df861d5db640d3d4
Akhilesh26 <akhileshpimple3@gmail.com>
2024-10-09 22:45:49 +0530

    create function AddnewResult and ShowAllData

--- Backend/Controller/SemesterResult.js
+++ Backend/Controller/SemesterResult.js
@@ -1,9 +1,16 @@
 const {semResult} = require("../Model/SemesterData");
 
 async function AddNewResult(data) {
-    const reuslt =  await semResult.create(data);
+    const result =  await semResult.create(data);
+    return result;
+}
+
+async function ShowAllData(){
+    const result = await semResult.find();
+    return result;
 }
 
 module.exports = {
     AddNewResult,
+    ShowAllData,
 }
