7599c6fc77f26622ccbf73ca32e52c677dfae979
Akhilesh26 <akhileshpimple3@gmail.com>
2024-10-20 11:48:58 +0530

    add new desgin changes in git header

--- Frontend/src/Components/Header.jsx
+++ Frontend/src/Components/Header.jsx
@@ -1,11 +1,14 @@
-const Header = () =>{
+const Header = () => {
     return (
         <>
-        <div>
-            <div>SemHolder</div>
-            <button>About Dev</button>
-            <button>Tech</button>
-        </div>
+            <div className="flex justify-between bg-[#9cf6f6] py-5 px-2">
+                <div className="text-2xl text-[#8f2d56]  ">SemHolder</div>
+                <div>
+
+                    <button className="font-bold text-lg rounded-lg border-2 border-sky-950 py-1 px-2 mx-2 text-sky-50 bg-[#f05d5e]">About Dev</button>
+                    <button className="font-bold text-lg rounded-lg border-2 border-sky-950 py-1 px-2 mx-2 text-sky-50 bg-[#f05d5e]">Tech</button>
+                </div>
+            </div>
         </>
     )
 };
