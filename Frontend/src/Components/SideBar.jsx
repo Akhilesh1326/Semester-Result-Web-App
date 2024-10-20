3f725d6e7bce609ee9f2d9ccaab6cd7d1b9d663a
Akhilesh26 <akhileshpimple3@gmail.com>
2024-10-20 11:49:11 +0530

    add desgin in git repo

--- Frontend/src/Components/SideBar.jsx
+++ Frontend/src/Components/SideBar.jsx
@@ -70,37 +70,37 @@ const SideBar = () => {
         }
     }
 
-        async function updateResult() {  // Fixed typo in function name
-            console.log(userName,
+    async function updateResult() {  // Fixed typo in function name
+        console.log(userName,
+            PRN,
+            rollNumber,
+            division,
+            marksOfCNT,
+            marksOfCC,
+            marksOfANN,
+        );
+        try {
+            const resp = await axios.put("/api/user/UpdateResultInfo", {
+                userName,
                 PRN,
                 rollNumber,
                 division,
+                marksOfDAA,
                 marksOfCNT,
                 marksOfCC,
                 marksOfANN,
-            );
-            try {
-                const resp = await axios.put("/api/user/UpdateResultInfo", {
-                    userName,
-                    PRN,
-                    rollNumber,
-                    division,
-                    marksOfDAA,
-                    marksOfCNT,
-                    marksOfCC,
-                    marksOfANN,
-                });
-                console.log("Update response = ", resp);
-                if (resp.data.msg === "Good") {
-                    setLoad(false);
-                } else {
-                    setLoad(true);
-                }
-            } catch (error) {
-                console.log("Error while updating info:", error);
+            });
+            console.log("Update response = ", resp);
+            if (resp.data.msg === "Good") {
+                setLoad(false);
+            } else {
+                setLoad(true);
             }
+        } catch (error) {
+            console.log("Error while updating info:", error);
         }
-    
+    }
+
 
 
     useEffect(() => {
@@ -121,97 +121,105 @@ const SideBar = () => {
     }
 
     return (
-        <div>
-            <div>
+        <div className="bg-[#0a2342]">
+    
+            <div className="text-2xl mx-4 ">
                 All Operations
             </div>
-            <button onClick={() => { setChooseButton(1), displayChange() }}>Create New Result Holder</button>
-            <button onClick={() => { setChooseButton(2), displayChange() }}>Update Existing Result Holder</button>
-            <button onClick={() => { setChooseButton(3), displayChange(), setDragData(!dragData) }}>Delete Exiting Result Holder</button>
-            {chooseButton == 1 ? (<>
-                <div>Add new result</div>
-                <div>Enter Name</div>
-                <input type="text" name="" id="" placeholder="Name" value={userName} onChange={(e) => { setUserName(e.target.value) }} />
-                <div>Enter PRN</div>
-                <input type="text" name="" id="" placeholder="PRN" value={PRN} onChange={(e) => { setPRN(e.target.value) }} />
-                <div>Enter ROLL</div>
-                <input type="text" name="" id="" placeholder="Roll" value={rollNumber} onChange={(e) => { setRollNumber(e.target.value) }} />
-                <div>Enter Division</div>
-                <input type="text" name="" id="" placeholder="Div" value={division} onChange={(e) => { setDivision(e.target.value) }} />
-                <div>Enter Marks Of DAA</div>
-                <input type="text" name="" id="" placeholder="DAA Marks" value={marksOfDAA} onChange={(e) => { setMarksOfDAA(e.target.value) }} />
-                <div>Enter Marks Of CNT</div>
-                <input type="text" name="" id="" placeholder="CNT Marks" value={marksOfCNT} onChange={(e) => { setMarksOfCNT(e.target.value) }} />
-                <div>Enter Marks Of ANN</div>
-                <input type="text" name="" id="" placeholder="ANN Marks" value={marksOfANN} onChange={(e) => { setMarksOfANN(e.target.value) }} />
-                <div>Enter Marks Of CC</div>
-                <input type="text" name="" id="" placeholder="CC Marks" value={marksOfCC} onChange={(e) => { setMarksOfCC(e.target.value) }} />
-                <button onClick={addSemResults}>{load ? "|" : "Submit"}</button>
-            </>) :
-                chooseButton == 2 ? (<>
-                    <div>Update new result</div>
-                    <input type="text" placeholder="Enter Student PRN to Modify Data" value={reqPRN} onChange={(e) => setReqPRN(e.target.value)} />
-                    <button onClick={() => getSingleUserData()}>Submit PRN</button>
-                    <div>
-                        <div>Name: <strong>{userName}</strong></div>
-                        <input type="text" name="" id="" placeholder="Name" value={userName} onChange={(e) => { setUserName(e.target.value) }} />
+            <div className="flex justify-center">
+                <button className="border-sky-950 border-2 mx-2 py-1 px-2 my-5 rounded-lg bg-[#5c95ff] text-sky-50" onClick={() => { setChooseButton(1), displayChange() }}>Create New Result Holder</button>
+                <button className="border-sky-950 border-2 mx-2 py-1 px-2 my-5 rounded-lg bg-[#5c95ff] text-sky-50" onClick={() => { setChooseButton(2), displayChange() }}>Update Existing Result Holder</button>
+                <button className="border-sky-950 border-2 mx-2 py-1 px-2 my-5 rounded-lg bg-[#5c95ff] text-sky-50" onClick={() => { setChooseButton(3), displayChange(), setDragData(!dragData) }}>Delete Exiting Result Holder</button>
+            </div>
+            <div className="flex flex-col justify-center items-center ">
+                {chooseButton == 1 ? (<>
 
-                    </div>
-                    <div>
-                        <div>PRN: <strong>{PRN}</strong></div>
-                        <input type="text" name="" id="" placeholder="PRN" value={PRN} onChange={(e) => { setPRN(e.target.value) }} />
 
-                    </div>
-                    <div>
-                        <div>Roll Number: <strong>{rollNumber}</strong></div>
-                        <input type="text" name="" id="" placeholder="Roll" value={rollNumber} onChange={(e) => { setRollNumber(e.target.value) }} />
-
-                    </div>
-                    <div>
-                        <div>Division: <strong>{division}</strong></div>
-                        <input type="text" name="" id="" placeholder="Div" value={division} onChange={(e) => { setDivision(e.target.value) }} />
-
-                    </div>
-                    <div>
-                        <div>Marks of DAA: <strong>{marksOfDAA}</strong></div>
-                        <input type="text" name="" id="" placeholder="DAA Marks" value={marksOfDAA} onChange={(e) => { setMarksOfDAA(e.target.value) }} />
-
-                    </div>
-                    <div>
-                        <div>Marks of CNT: <strong>{marksOfCNT}</strong></div>
-                        <input type="text" name="" id="" placeholder="CNT Marks" value={marksOfCNT} onChange={(e) => { setMarksOfCNT(e.target.value) }} />
-
-                    </div>
-                    <div>
-                        <div>Marks of ANN: <strong>{marksOfANN}</strong></div>
-                        <input type="text" name="" id="" placeholder="ANN Marks" value={marksOfANN} onChange={(e) => { setMarksOfANN(e.target.value) }} />
-
-                    </div>
-                    <div>
-                        <div>Marks of CC: <strong>{marksOfCC}</strong></div>
-                        <input type="text" name="" id="" placeholder="CC Marks" value={marksOfCC} onChange={(e) => { setMarksOfCC(e.target.value) }} />
-
-                    </div>
-                    <button onClick={()=>{updateResult()}}>{load ? "|" : "Update Data"}</button>
-
+                    <div className="my-2 ">Add new result</div>
+                    <div className="my-2 ">Enter Name</div>
+                    <input className="border-2 border-sky-950 py-1 px-2 rounded-lg bg-gray-200" type="text" name="" id="" placeholder="Name" value={userName} onChange={(e) => { setUserName(e.target.value) }} />
+                    <div className="my-2 ">Enter PRN</div>
+                    <input className="border-2 border-sky-950 py-1 px-2 rounded-lg bg-gray-200" type="text" name="" id="" placeholder="PRN" value={PRN} onChange={(e) => { setPRN(e.target.value) }} />
+                    <div className="my-2 ">Enter ROLL</div>
+                    <input className="border-2 border-sky-950 py-1 px-2 rounded-lg bg-gray-200" type="text" name="" id="" placeholder="Roll" value={rollNumber} onChange={(e) => { setRollNumber(e.target.value) }} />
+                    <div className="my-2 ">Enter Division</div>
+                    <input className="border-2 border-sky-950 py-1 px-2 rounded-lg bg-gray-200" type="text" name="" id="" placeholder="Div" value={division} onChange={(e) => { setDivision(e.target.value) }} />
+                    <div className="my-2 ">Enter Marks Of DAA</div>
+                    <input className="border-2 border-sky-950 py-1 px-2 rounded-lg bg-gray-200" type="text" name="" id="" placeholder="DAA Marks" value={marksOfDAA} onChange={(e) => { setMarksOfDAA(e.target.value) }} />
+                    <div className="my-2 ">Enter Marks Of CNT</div>
+                    <input className="border-2 border-sky-950 py-1 px-2 rounded-lg bg-gray-200" type="text" name="" id="" placeholder="CNT Marks" value={marksOfCNT} onChange={(e) => { setMarksOfCNT(e.target.value) }} />
+                    <div className="my-2 ">Enter Marks Of ANN</div>
+                    <input className="border-2 border-sky-950 py-1 px-2 rounded-lg bg-gray-200" type="text" name="" id="" placeholder="ANN Marks" value={marksOfANN} onChange={(e) => { setMarksOfANN(e.target.value) }} />
+                    <div className="my-2 ">Enter Marks Of CC</div>
+                    <input className="border-2 border-sky-950 py-1 px-2 rounded-lg bg-gray-200" type="text" name="" id="" placeholder="CC Marks" value={marksOfCC} onChange={(e) => { setMarksOfCC(e.target.value) }} />
+                    <button onClick={addSemResults}>{load ? "|" : "Submit"}</button>
                 </>) :
-                    (<>
-                        {allSemData == "No Data Found" ? (<>Data Not Found</>) : (<>
-                            {allSemData.map((item) => (
-                                <div key={item._id} className="p-4 border-b">
-                                    <div>Name: <strong>{item.userName}</strong></div>
-                                    <div>PRN: <strong>{item.PRN}</strong></div>
-                                    <div>Roll Number: <strong>{item.rollNumber}</strong></div>
-                                    <div>Division: <strong>{item.division}</strong></div>
-                                    <div>Marks of DAA: <strong>{item.marksOfDAA}</strong></div>
-                                    <div>Marks of CNT: <strong>{item.marksOfCNT}</strong></div>
-                                    <div>Marks of ANN: <strong>{item.marksOfANN}</strong></div>
-                                    <div>Marks of CC: <strong>{item.marksOfCC}</strong></div>
-                                </div>
-                            ))}
+                    chooseButton == 2 ? (<>
+                        <div>Update new result</div>
+                        <input type="text" placeholder="Enter Student PRN to Modify Data" value={reqPRN} onChange={(e) => setReqPRN(e.target.value)} />
+                        <button onClick={() => getSingleUserData()}>Submit PRN</button>
+                        <div>
+                            <div>Name: <strong>{userName}</strong></div>
+                            <input type="text" name="" id="" placeholder="Name" value={userName} onChange={(e) => { setUserName(e.target.value) }} />
 
+                        </div>
+                        <div>
+                            <div>PRN: <strong>{PRN}</strong></div>
+                            <input type="text" name="" id="" placeholder="PRN" value={PRN} onChange={(e) => { setPRN(e.target.value) }} />
+
+                        </div>
+                        <div>
+                            <div>Roll Number: <strong>{rollNumber}</strong></div>
+                            <input type="text" name="" id="" placeholder="Roll" value={rollNumber} onChange={(e) => { setRollNumber(e.target.value) }} />
+
+                        </div>
+                        <div>
+                            <div>Division: <strong>{division}</strong></div>
+                            <input type="text" name="" id="" placeholder="Div" value={division} onChange={(e) => { setDivision(e.target.value) }} />
+
+                        </div>
+                        <div>
+                            <div>Marks of DAA: <strong>{marksOfDAA}</strong></div>
+                            <input type="text" name="" id="" placeholder="DAA Marks" value={marksOfDAA} onChange={(e) => { setMarksOfDAA(e.target.value) }} />
+
+                        </div>
+                        <div>
+                            <div>Marks of CNT: <strong>{marksOfCNT}</strong></div>
+                            <input type="text" name="" id="" placeholder="CNT Marks" value={marksOfCNT} onChange={(e) => { setMarksOfCNT(e.target.value) }} />
+
+                        </div>
+                        <div>
+                            <div>Marks of ANN: <strong>{marksOfANN}</strong></div>
+                            <input type="text" name="" id="" placeholder="ANN Marks" value={marksOfANN} onChange={(e) => { setMarksOfANN(e.target.value) }} />
+
+                        </div>
+                        <div>
+                            <div>Marks of CC: <strong>{marksOfCC}</strong></div>
+                            <input type="text" name="" id="" placeholder="CC Marks" value={marksOfCC} onChange={(e) => { setMarksOfCC(e.target.value) }} />
+
+                        </div>
+                        <button onClick={() => { updateResult() }}>{load ? "|" : "Update Data"}</button>
+
+                    </>) :
+                        (<>
+                            {allSemData == "No Data Found" ? (<>Data Not Found</>) : (<>
+                                {allSemData.map((item) => (
+                                    <div key={item._id} className="p-4 border-b">
+                                        <div>Name: <strong>{item.userName}</strong></div>
+                                        <div>PRN: <strong>{item.PRN}</strong></div>
+                                        <div>Roll Number: <strong>{item.rollNumber}</strong></div>
+                                        <div>Division: <strong>{item.division}</strong></div>
+                                        <div>Marks of DAA: <strong>{item.marksOfDAA}</strong></div>
+                                        <div>Marks of CNT: <strong>{item.marksOfCNT}</strong></div>
+                                        <div>Marks of ANN: <strong>{item.marksOfANN}</strong></div>
+                                        <div>Marks of CC: <strong>{item.marksOfCC}</strong></div>
+                                    </div>
+                                ))}
+
+                            </>)}
                         </>)}
-                    </>)}
+            </div>
+
         </div>
     )
 };
